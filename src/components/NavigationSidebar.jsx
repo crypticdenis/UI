5b401c4ab04ea3b58d52d476bbe5477d491e81df
Denis Cvach <d.cvach@enbw.com>
2025-11-11 12:08:09 +0100

    Remove home icon from NavigationSidebar to fix overlap with collapse arrow

--- src/components/NavigationSidebar.jsx
+++ src/components/NavigationSidebar.jsx
@@ -149,10 +149,6 @@ const NavigationSidebar = ({
       
       <div className="sidebar-header">
         <div className="sidebar-title">
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
-            <polyline points="9 22 9 12 15 12 15 22"/>
-          </svg>
           {!isCollapsed && <span>Explorer</span>}
         </div>
         <button 
